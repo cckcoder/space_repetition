afcde3fafd254f2add7386cff241ba73a16d92a1
dev2 <c.chaiwat.ch@gmail.com>
2021-04-03 12:32:36 +0700

    update: export script
    Signed-off-by: dev2 <c.chaiwat.ch@gmail.com>

--- dockers/export_db.sh
+++ dockers/export_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -i --rm  --net=host -e PGPASSWORD=dev2original  postgres /usr/bin/pg_dump -h localhost -U root btredb | gzip -9 -f > init/btredb.sql.gz
+docker run -i --rm --net=host -e PGPASSWORD=dev2original postgres /usr/bin/pg_dump -h localhost -p 5343 -U root space_repetition | gzip -9 -f > init/space_repetition.sql.gz
 
